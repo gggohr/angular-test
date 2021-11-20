982c014b35e57d8729ff6ec19ca3c37aed39181d
Greg Gohr <gregory.gohr@gmail.com>
2021-11-20 14:21:09 -0600

    Exercise 2

---
+++ my-app/src/app/mock-heros.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 100, name: 'Dr Lovegood' },
+  { id: 101, name: 'James Bond' },
+  { id: 102, name: 'Mario' },
+  { id: 103, name: 'Luigi' },
+  { id: 104, name: 'Batman' },
+  { id: 105, name: 'Bomberman' },
+  { id: 106, name: 'Robin' },
+  { id: 107, name: 'Dr No' },
+  { id: 108, name: 'Jaws' },
+  { id: 109, name: 'Aquaman' }
+];
